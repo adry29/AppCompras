52ddd0bc2640e4b6040209bb2ef1be706ac4b372
adry29 <adrycrack@gmail.com>
2021-11-07 23:16:54 +0100

    Parte1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     projectId: 'appcompras-89748',
     appId: '1:903429107528:web:589e9f7f94566eb3464d7d',
